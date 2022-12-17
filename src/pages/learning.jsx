a5155d203ecdd1232ae02ed33346f43461aed439
Pochi Chao <19pochi94@gmail.com>
2022-12-17 13:38:50 -0600

    first commit

---
+++ src/pages/learning.jsx
@@ -0,0 +1,104 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function LearningSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-10">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Learning({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export default function Learnings() {
+  return (
+    <>
+      <Head>
+        <title>Learnings - Pochi Chao</title>
+        <meta
+          name="description"
+          content="What I've used for learning coding / programming."
+        />
+      </Head>
+      <SimpleLayout
+        title="What I've used for learning coding / programming."
+        intro=""
+      >
+        <div className="space-y-16">
+          <LearningSection title="Online Courses">
+            <Learning title="Udemy - 'The Complete 2023 Web Development Bootcamp' by Angela Yu">
+              <Link
+                href="https://www.udemy.com/course/the-complete-web-development-bootcamp/"
+                className="text-blue-400"
+              >
+                Covers HTML, CSS, JavaScript, Node + Express, React, MongoDB, and some Web3 stuff.
+              </Link>
+            </Learning>
+            <Learning title="Udemy - '100 Days of Code: The Complete Python Pro Bootcamp for 2023' by Angela Yu">
+              <Link
+                href="https://www.udemy.com/course/100-days-of-code/"
+                className="text-blue-400"
+              >
+                Covers Python, Flask, Pandas.
+              </Link>
+            </Learning>
+          </LearningSection>
+          <LearningSection title="One-on-One Mentoring Sessions via Zoom">
+            <Learning title="Playground Technology School by wenquai">
+              I had the privilege of attending the {" "} 
+              <Link
+                href="https://onplayground.notion.site/Playground-Technology-School-176eef9dcdc14adcb4d66b280b7b0406"
+                className="text-blue-400"
+              >
+                pilot program for Playground
+              </Link>
+               {" "} since I was friends with wenquai. 
+              Through a well-crafted, project-oriented curriculum and our weekly Zoom sessions, I greatly reinforced 
+              my knowledge of HTML, CSS, JavaScript, and React from the Web Dev Udemy course. I also learned about
+              NextJS, TypeScript, TailwindCSS, OpenAI, common APIs like NextAuth, and more!
+            </Learning>
+          </LearningSection>
+          <LearningSection title="YouTube Channels">
+            <Learning title="Fireship">
+              To round out the "hard" learning sessions and reduce the fatigue of rapid learning for new coders, {" "}
+              <Link
+                href="https://www.youtube.com/@Fireship"
+                className="text-blue-400"
+              >
+                Fireship
+              </Link>
+               {" "} is such a great channel for "soft" learning through entertaining yet info-packed videos. 
+            </Learning>
+          </LearningSection>
+          <LearningSection title="Troubleshooting Refs other than StackOverflow">
+            <Learning title="BobbyHadz">
+              So, so many common web development bugs are catalogued in {" "}
+              <Link
+                href="https://bobbyhadz.com/"
+                className="text-blue-400"
+              >
+                BobbyHadz' website
+              </Link>
+               {" "}, and his website and articles have a super clean UI with clear, thorough explanations of bugs. 
+            </Learning>
+          </LearningSection>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
---
+++ src/pages/tools.jsx
@@ -0,0 +1,100 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-10">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export default function Uses() {
+  return (
+    <>
+      <Head>
+        <title>Tools - Pochi Chao</title>
+        <meta
+          name="description"
+          content="Tools for the job."
+        />
+      </Head>
+      <SimpleLayout
+        title="Tools for the job."
+        intro=""
+      >
+        <div className="space-y-16">
+          <ToolsSection title="Workstation">
+            <Tool title='M1 Macbook Air with 8GB RAM, 256GB SSD'>
+              More than enough power for my level of development while also
+              having outstanding battery life within a light chassis.
+            </Tool>
+            <Tool title='27" Dell IPS LED QHD Monitor for home use'>
+              Big screen for big developing. I pair this with a {" "}
+              <Link
+                href="https://www.amazon.com/gp/product/B075JYG2TB/ref=ppx_yo_dt_b_asin_title_o09_s00?ie=UTF8&th=1"
+                className="text-blue-400"
+              >
+                32" stand-up desk converter from Amazon.
+              </Link>
+            </Tool>
+            <Tool title='15" Asus Portable Monitor for on-the-go development'>
+              <Link
+                href="https://www.amazon.com/gp/product/B09MZQ978N/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1"
+                className="text-blue-400"
+              >
+                1080p portable monitor with a wide screen
+              </Link>
+              , handy for the long lines of TailwindCSS code. Excellent value!
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Development tools">
+            <Tool title="VS Code with Prettier">The gold standard.</Tool>
+            <Tool title="iTerm2">
+              Simple and clean UI, and I personally prefer having a separate
+              window for my terminal vs. the built-in one in VS Code.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Design">
+            <Tool title="Figma">
+              The whiteboarding section of the website is very handy for quick
+              lo-fi wireframing and diagramming.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Productivity">
+            <Tool title="The Arc Browser">
+              Having separate spaces for coding and productive time vs. casual
+              and fun time is genuinely so useful. Being inside the productive
+              space acts as a strong deterrent for clicking away onto social
+              media or news websites and killing time. I feel that I must click
+              away and switch spaces to avoid corrupting my productive space if
+              I am to waste time, and that has seemingly small obstacle keeps me
+              making the responsible decision.
+            </Tool>
+            <Tool title="Rocketbook Fusion">
+              For when the tactile feedback of physically writing or diagramming
+              problems is needed. The ability to quickly scan pages and send them 
+              to 7 different programmable destinations is very useful too.
+            </Tool>
+          </ToolsSection>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
