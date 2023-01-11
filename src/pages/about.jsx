9f977f366bd49b0c89bf6bb6d80f9e04cb932f68
Pochi Chao <19pochi94@gmail.com>
2023-01-11 08:33:44 -0600

    update about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -66,7 +66,7 @@ export default function About() {
               <p>
                 I left my job at the Phillips 66 Sweeny Refinery in May 2022, and I began learning HTML/CSS/JavaScript soon after.
                 With the help of weekly mentoring sessions from a brilliant data scientist friend and some Udemy courses, I&apos;ve learned 
-                about React, NextJS, Tailwind, TypeScript, and other useful frameworks and libraries.
+                about React, NextJS, Tailwind, TypeScript, and other useful frameworks and libraries. I'm currently learning Python.
               </p>
               <p>
                 The problems and fields that currently interest me are: clean energy technologies, 
